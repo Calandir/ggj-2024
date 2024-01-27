9c8094754775ee296b2e2d859f0fa6db9f6e5aa9
VUWnashjame2 <nashjame2@myvuw.ac.nz>
2024-01-27 20:47:37 +1300

    Add more varieties of Fish

--- Assets/Scripts/SpawnFish.cs
+++ Assets/Scripts/SpawnFish.cs
@@ -1,8 +1,14 @@
+using System;
 using UnityEngine;
 
 public class SpawnFish : MonoBehaviour
 {
     public GameObject fish;
+    public GameObject longfish;
+    public GameObject bluefish;
+    public GameObject pinkfish;
+    public GameObject orangefish;
+    public GameObject catfish;
     [SerializeField] public bool facingRight;
     Vector2 startPosition;
 
@@ -15,7 +21,41 @@ public class SpawnFish : MonoBehaviour
 
     void Spawn()
     {
-        GameObject instantiatedFish = Instantiate(fish, transform.position, Quaternion.identity);
+        GameObject fishToSpawn = null;
+        float randFloat = (int)Math.Ceiling(UnityEngine.Random.Range(0.0f, 1.0f) * 10);
+        switch(randFloat)
+        {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                fishToSpawn = fish;
+                break;
+            case 5:
+                fishToSpawn = longfish;
+                break;
+            case 6:
+            case 7:
+                fishToSpawn = bluefish;
+                break;
+            case 8:
+                fishToSpawn = orangefish;
+                break;
+
+            case 9:
+                if (UnityEngine.Random.Range(0.0f, 1.0f) > 0.3f && transform.position.y < -3) {fishToSpawn = catfish;}
+                else {fishToSpawn = fish;}
+                break;
+            case 10:
+                if (UnityEngine.Random.Range(0.0f, 1.0f) > 0.95f) {fishToSpawn = pinkfish;}
+                else {fishToSpawn = fish;}
+                break;
+            default:
+                fishToSpawn = fish;
+                break;
+        }
+        
+        GameObject instantiatedFish = Instantiate(fishToSpawn, transform.position, Quaternion.identity);
         instantiatedFish.GetComponent<FishMover>().SetDirection(facingRight);
     }
 
