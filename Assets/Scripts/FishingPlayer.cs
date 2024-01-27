16b53e3e51ad58af91d3a7921382cb7ca614d6df
Antony Thomas <antony@thomas-cgi.com>
2024-01-27 15:42:39 +1300

    Add menu bar for rod charge

--- Assets/Scripts/FishingPlayer.cs
+++ Assets/Scripts/FishingPlayer.cs
@@ -21,8 +21,6 @@ public class FishingPlayer : MonoBehaviour
 	public float CastPower = 0;
 	// How fast it takes to charge max power in seconds.
 	public float CastChargeSpeed;
-	// Store space bar state.
-	private bool spaceBarDown = false;
 
 	public enum FishingState
 	{
@@ -43,19 +41,15 @@ public class FishingPlayer : MonoBehaviour
 	{
 		if (m_currentState == FishingState.ChargeCast)
 		{
-			if (Input.GetKeyDown(KeyCode.Space)) {
-				spaceBarDown = true;
-			} else if (Input.GetKeyUp(KeyCode.Space)) {
+			if (Input.GetKeyUp(KeyCode.Space)) {
 				// When space bar is released, cast rod
-				spaceBarDown = false;
 				m_currentState = FishingState.Cast;
 			}
 
-			if (spaceBarDown) {
+			if (Input.GetKey(KeyCode.Space)) {
 				// While space bar is held down, increase cast power.
 				CastPower += 1.0f / (CastChargeSpeed / 100.0f) * Time.deltaTime;
 				CastPower = Mathf.Clamp(CastPower, 0.0f, 100.0f);
-
 			}
 		}
 		else if (m_currentState == FishingState.Cast)
