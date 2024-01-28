a38cea5007eb52a8567787723c2840485a9b8988
VUWnashjame2 <nashjame2@myvuw.ac.nz>
2024-01-28 14:33:32 +1300

    Add wiggle to swimming fish
    Fish now move a small amount in a random direction as they swim at set intervals

--- Assets/Scripts/FishMover.cs
+++ Assets/Scripts/FishMover.cs
@@ -45,6 +45,7 @@ public class FishMover : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         SetDirection(facingRight);
+        InvokeRepeating("Wiggle", 0, 0.4f);
     }
     
     public void SetDirection(bool directionIsRight)
@@ -59,7 +60,20 @@ public class FishMover : MonoBehaviour
                 transform.localScale = new UnityEngine.Vector2(-0.7f, 0.7f);
             }
     }
-    
+    private void Wiggle()
+    {
+        if (m_isHooked) {
+            return;
+        }
+        float randX = UnityEngine.Random.Range(-0.1f, 0.1f);
+        float randY = UnityEngine.Random.Range(-0.1f, 0.1f);
+        randX += transform.position.x;
+        if (transform.position.y > -0.5) {randY = -0.1f;}
+        else if (transform.position.y < -7.5) {randY = 0.1f;}
+        randY += transform.position.y;
+        
+        transform.position = new UnityEngine.Vector2(randX, randY);
+    }
     private void FixedUpdate()
     {
         if (m_isHooked)
