b8a434b88f58d8a8640ea22929a269c324f0e35e
Kilreth <jonathan.pavat.brown@gmail.com>
2024-01-28 16:03:50 +1300

    Add line stretch SFX on hooking a fish

--- Assets/Scripts/SFXSingleton.cs
+++ Assets/Scripts/SFXSingleton.cs
@@ -7,22 +7,25 @@ public class SFXSingleton : MonoBehaviour
 
 	public AudioSource AudioSource;
 
-    public AudioClip PlipClip1;
-    public AudioClip PlipClip2;
-    public AudioClip PlipClip3;
+	public AudioClip PlipClip1;
+	public AudioClip PlipClip2;
+	public AudioClip PlipClip3;
 
-    public AudioClip SplashClip1;
+	public AudioClip SplashClip1;
 	public AudioClip SplashClip2;
 	public AudioClip SplashClip3;
 
-	public AudioClip RodClip;
+	public AudioClip LineStretchClip1;
+	public AudioClip LineStretchClip2;
+	public AudioClip LineStretchClip3;
 
 	public AudioClip ThunkClip1;
 	public AudioClip ThunkClip2;
 	public AudioClip ThunkClip3;
 
-    private AudioClip[] m_plipClips;
-    private AudioClip[] m_splashClips;
+	private AudioClip[] m_plipClips;
+	private AudioClip[] m_splashClips;
+	private AudioClip[] m_lineStretchClips;
 	private AudioClip[] m_thunkClips;
 
 	private void Awake()
@@ -35,10 +38,12 @@ public class SFXSingleton : MonoBehaviour
 
 		s_instance = this;
 
-        m_plipClips = new AudioClip[] {
-            PlipClip1, PlipClip2, PlipClip3};
-        m_splashClips = new AudioClip[] {
-            SplashClip1, SplashClip2, SplashClip3};
+		m_plipClips = new AudioClip[] {
+			PlipClip1, PlipClip2, PlipClip3};
+		m_splashClips = new AudioClip[] {
+			SplashClip1, SplashClip2, SplashClip3};
+		m_lineStretchClips = new AudioClip[] {
+			LineStretchClip1, LineStretchClip2, LineStretchClip3};
 		m_thunkClips = new AudioClip[] {
 			ThunkClip1, ThunkClip2, ThunkClip3};
 	}
@@ -47,22 +52,27 @@ public class SFXSingleton : MonoBehaviour
 	{
 		int random = Random.Range(0, clips.Length);
 		AudioSource.PlayOneShot(clips[random], volumeScale);
-    }
+	}
 
-    public void PlayRodSFX(float volumeScale = 1.0f)
-    {
-        AudioSource.PlayOneShot(RodClip, volumeScale);
-    }
+	//public void PlayRodSFX(float volumeScale = 1.0f)
+	//{
+	//    AudioSource.PlayOneShot(RodClip, volumeScale);
+	//}
 
-    public void PlayPlipSFX(float volumeScale = 1.0f)
+	public void PlayPlipSFX(float volumeScale = 1.0f)
 	{
 		PlayRandomSFX(m_plipClips, volumeScale);
 	}
 
 	public void PlaySplashSFX(float volumeScale = 1.0f)
-    {
-        PlayRandomSFX(m_splashClips, volumeScale);
-    }
+	{
+		PlayRandomSFX(m_splashClips, volumeScale);
+	}
+
+	public void PlayLineStretchSFX(float volumeScale = 1.0f)
+	{
+		PlayRandomSFX(m_lineStretchClips, volumeScale);
+	}
 
 	public void PlayThunkSFX(float volumeScale = 1.0f)
 	{
